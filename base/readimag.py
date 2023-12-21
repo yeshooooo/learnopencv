2808a9e932453c6696a500dc96e73416c75ea15d
yeshooo <81931672@qq.com>
2023-12-21 12:03:04 +0800

    程序退出逻辑

--- base/readimag.py
+++ base/readimag.py
@@ -8,7 +8,12 @@ img = cv2.imread("base/static/perspective.jpeg")
 # 将图片和窗口绑定到一起
 cv2.imshow("img", img)
 key = cv2.waitKey(0)
+print(key)
+print('q')
+print(ord('q')) # ord()获取字符的ascii码
+# if(key == 'q'):
+# 因为ascii码是8位的，所以使用位运算只获取最后一个8位
+if(key & 0xFF== ord('q')):
+    print(11111) #这里实际上并没有执行,应改为ord("q")
+    cv2.destroyAllWindows()
 
-if(key == 'q'):
-    exit()
-cv2.destroyAllWindows()
