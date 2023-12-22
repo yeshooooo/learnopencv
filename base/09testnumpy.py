a475574a48641ec96197700c1d247620c6e564b4
yeshooo <81931672@qq.com>
2023-12-23 00:15:28 +0800

    numpy

--- base/09testnumpy.py
+++ base/09testnumpy.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import cv2
 # 通过array 定义矩阵
 a = np.array([1, 2, 3])
 b = np.array([[1, 2, 3], [4, 5, 6]])
@@ -26,4 +26,20 @@ f = np.identity(8)
 # eye矩阵
 # key是从第几个开始(从0开始计数)
 g = np.eye(5, 7, 3)
-print(g)
+# print(g)
+
+img = np.zeros((480, 640, 3), np.uint8)
+
+# count = 0
+# while count < 200:
+#     img[count, 100, 0] = 255
+#     count += 1
+
+roi = img[100:400, 100:600]
+roi[:,:] = [0, 0, 255]
+roi[10:200, 10:200] = [0, 255, 0]
+
+cv2.imshow('img', img)
+key = cv2.waitKey(0)
+if key & 0xFF == ord('q'):
+    cv2.destroyAllWindows()
